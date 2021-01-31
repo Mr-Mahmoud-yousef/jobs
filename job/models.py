a827b943e639dec2d081aeca0375ecadcc95267a
Mahmoud <mohamadmostfa869@gmail.com>
2021-01-31 21:12:58 +0200

    add job detal

--- job/models.py
+++ job/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+worktime=(
+    ('Full time','Full time'),
+    ('Part time','Part time'),
+)
+class Job(models.Model):
+    title=models.CharField(max_length=50)
+    job_type=models.CharField(max_length=10,choices=worktime)
+    description=models.TextField(max_length=500)
+    date=models.DateTimeField(auto_now=True)
+    Vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(max_length=2)
+
+
+
+    def __str__(self):
+        return(self.title)
+        
